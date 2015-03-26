cd4d8642deee26be2dd8f6cf967059730e2eae36
Arpit Agrawal <arpit.agrawal.27@gmail.com>
2015-03-26 18:17:14 +0530

    works for mobile devices without sharing

--- app.js
+++ app.js
@@ -1,65 +1,58 @@
 var fs = require('fs');
-var regexp = require('node-regexp');
-var express = require('express');
-var bodyParser = require("body-parser");
-var http = require('http');
-var request = require('request');
-var util = require('util');
-var spawn = require('child_process').spawn;
-
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-var re = regexp().end('.mp4').toRegExp();
-// var re1=regexp().end('.flv').toRegExp(),re2=regexp().end('.mkv').toRegExp(),re3=regexp().end('.ogg').toRegExp(),re5=regexp().end('.dctmp').toRegExp();
-
-
+var regexp=require('node-regexp');
+var re = regexp().end('.mp4').toRegExp(),re1=regexp().end('.flv').toRegExp(),re2=regexp().end('.mkv').toRegExp(),re3=regexp().end('.ogg').toRegExp(),re5=regexp().end('.dctmp').toRegExp();
+var express=require('express');
+var bodyParser     =         require("body-parser");
+var app=express();
 var map={},map1={},map3={};
-var co=0;
-var has_conn=0;
-var search_req=0;
-var map={};
-var cur_mov_str;
+var http = require('http'),fs = require('fs'),util = require('util');
+var request=require('request');
+var util  = require('util'),
+    spawn = require('child_process').spawn;
+    var co=0;
+    var has_conn=0;
+    var search_req=0;
+    var map={};
+    var cur_mov_str;
+ var http=require('http');
+ fs.writeFile("listofips", "", function(err) {
+    if(err) {
+        return console.log(err);
+    }
 
-fs.writeFile("listofips", "", function(err) {
-  if(err) {
-    return console.log(err);
-  }
-
-  console.log("The file was saved!");
+    console.log("The file was saved!");
 }); 
-
 function zeroFill(i) {
-  return (i < 10 ? '0' : '') + i;
+  return (i < 10 ? '0' : '') + i
 }
-
 function now () {
-  var d = new Date();
-  return d.getFullYear() + '-' + zeroFill(d.getMonth() + 1) + '-'
-          + zeroFill(d.getDate()) + ' '+ zeroFill(d.getHours()) 
-          + ':'+ zeroFill(d.getMinutes());
+   var d = new Date()
+   return d.getFullYear() + '-' + zeroFill(d.getMonth() + 1) + '-'+ zeroFill(d.getDate()) + ' '+ zeroFill(d.getHours()) + ':'+ zeroFill(d.getMinutes())
 }
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var spawn = require('child_process').spawn;
 var prc = spawn('python',  ['polling.py']);
 
 //noinspection JSUnresolvedFunction
 prc.stdout.setEncoding('utf8');
 prc.stderr.setEncoding('utf8');
 prc.stdout.on('data', function (data) {
-  var str = data.toString();
-  var lines = str.split(/(\r?\n)/g);
-  //console.log(lines.join(""));
+    var str = data.toString()
+    var lines = str.split(/(\r?\n)/g);
+    //console.log(lines.join(""));
 });
 
 prc.stderr.on('data', function (data) {
-  var str = data.toString();
-  var lines = str.split(/(\r?\n)/g);
-  // console.log(lines.join(""));
+    var str = data.toString()
+    var lines = str.split(/(\r?\n)/g);
+   // console.log(lines.join(""));
 });
 
 prc.on('close', function (code) {
-  //console.log('process exit code ' + code);
+    //console.log('process exit code ' + code);
 });
-
 app.get('/login', function (req, response) {
   var ip=req.connection.remoteAddress;
   console.log(ip);
@@ -107,6 +100,13 @@ app.get('/error', function (req, res) {
 
 });
 
+function sendSearchResponse(res) {
+  var str='<html><body><h1>Search!</h1><input type="TEXT" id="search" size="40"><button type="button" id="submit">Submit</button> <button onclick="parent.location=\'http://10.11.11.35:3001/allfiles\'">All-files</button></body><table id="table"></table>\n';
+  var js='<script>$(document).ready(function(){$("#submit").click(function(){var sea=$("#search").val();$.post("http://10.11.11.35:3001/search",{search: sea}, function(data){$("#table").html(data)});});})</script>\n';
+  var incl='<script src="http://10.11.11.35:3001/readjquery"></script></html>\n';
+  res.send(str+incl+js);
+}
+
 app.get('/search', function (req, response) {
    var ip=req.connection.remoteAddress;
   fs.appendFile("log", 'Search Request:'+ip+' '+now()+'\n', function(err) {
@@ -116,25 +116,33 @@ app.get('/search', function (req, response) {
 
     console.log("The file was saved!");
   }); 
-  http.get('http://'+ip+':3000/', function(res) {
-       console.log("Got response: " + res.statusCode);
-       res.on("data", function(chunk) {
-        if(chunk=='ok')
-        {
-          var str='<html><body><h1>Search!</h1><input type="TEXT" id="search" size="40"><button type="button" id="submit">Submit</button> <button onclick="parent.location=\'http://10.11.11.35:3001/allfiles\'">All-files</button></body><table id="table"></table>\n';
-          var js='<script>$(document).ready(function(){$("#submit").click(function(){var sea=$("#search").val();$.post("http://10.11.11.35:3001/search",{search: sea}, function(data){$("#table").html(data)});});})</script>\n';
-          var incl='<script src="http://10.11.11.35:3001/readjquery"></script></html>\n';
-          response.send(str+incl+js);
-        }
-        else
-        {
-          response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
-        }
-        });
-    }).on('error', function(e) {
-       console.log("Got error: " + e.message);
-       response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
-    });
+
+  var ua = req.headers['user-agent'];
+
+  if(/mobile/i.test(ua)) {
+    sendSearchResponse(response);
+  }
+  else {
+    http.get('http://'+ip+':3000/', function(res) {
+         console.log("Got response: " + res.statusCode);
+         res.on("data", function(chunk) {
+          if(chunk=='ok')
+          {
+            var str='<html><body><h1>Search!</h1><input type="TEXT" id="search" size="40"><button type="button" id="submit">Submit</button> <button onclick="parent.location=\'http://10.11.11.35:3001/allfiles\'">All-files</button></body><table id="table"></table>\n';
+            var js='<script>$(document).ready(function(){$("#submit").click(function(){var sea=$("#search").val();$.post("http://10.11.11.35:3001/search",{search: sea}, function(data){$("#table").html(data)});});})</script>\n';
+            var incl='<script src="http://10.11.11.35:3001/readjquery"></script></html>\n';
+            response.send(str+incl+js);
+          }
+          else
+          {
+            response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
+          }
+          });
+      }).on('error', function(e) {
+         console.log("Got error: " + e.message);
+         response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
+      });
+  }
 });
 
 app.post('/search', function (req, res) {
@@ -167,6 +175,11 @@ app.post('/search', function (req, res) {
     res.send(html);
 });
 
+function sendAllfilesResponse(res) {
+  var data=fs.readFileSync('allfiles.html','utf8');
+  res.send(data);
+}
+
 app.get('/allfiles', function (req, response) {
   var ip=req.connection.remoteAddress;
   fs.appendFile("log", 'All-Files Request:'+ip+' '+now()+'\n', function(err) {
@@ -176,23 +189,31 @@ app.get('/allfiles', function (req, response) {
 
     console.log("The file was saved!");
   }); 
-   http.get('http://'+ip+':3000/', function(res) {
-       console.log("Got response: " + res.statusCode);
-       res.on("data", function(chunk) {
-        if(chunk=='ok')
-        {
-           var data=fs.readFileSync('allfiles.html','utf8');
-           response.send(data);
-        }
-        else
-        {
-          response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
-        }
-        });
-    }).on('error', function(e) {
-       console.log("Got error: " + e.message);
-       response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
-    });
+
+  var ua = req.headers['user-agent'];
+
+  if(/mobile/i.test(ua)) {
+    sendAllfilesResponse(response);
+  }
+  else {
+     http.get('http://'+ip+':3000/', function(res) {
+         console.log("Got response: " + res.statusCode);
+         res.on("data", function(chunk) {
+          if(chunk=='ok')
+          {
+             var data=fs.readFileSync('allfiles.html','utf8');
+             response.send(data);
+          }
+          else
+          {
+            response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
+          }
+          });
+      }).on('error', function(e) {
+         console.log("Got error: " + e.message);
+         response.send('<html><meta http-equiv="refresh" content="0; url=http://10.11.11.35:3001/error?ip='+ip+'"></html>');
+      });
+  }
 });
 
 
